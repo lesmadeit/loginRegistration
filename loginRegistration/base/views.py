ee4a909f46bbe8531cd24e9ff6cab42f5c5f3a3c
lesmadeit <lesmwendwa@gmail.com>
2024-11-07 17:07:33 +0300

    changes

--- loginRegistration/base/views.py
+++ loginRegistration/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
@@ -9,3 +9,10 @@ def home(request):
 
 def authView(request):
     if request.method == "POST":
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect("base:login")
+        else:
+            form = UserCreationForm()
+        return render(request, "registration/signup.html", {"form": form})
