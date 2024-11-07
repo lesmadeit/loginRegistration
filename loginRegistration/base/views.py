1202e865dcbdeeeebd807883e1205bc9a17c08af
lesmadeit <lesmwendwa@gmail.com>
2024-11-07 16:49:33 +0300

    views

--- loginRegistration/base/views.py
+++ loginRegistration/base/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def home(request):
+    return render(request, "home.html", {})
+
+def authView(request):
+    if request.method == "POST":
