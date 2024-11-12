f66bd1e37425f4ee16be8bf53723a8d10264f730
lesmadeit <lesmwendwa@gmail.com>
2024-11-12 16:31:51 +0300

    forms

--- loginRegistration/base/forms.py
+++ loginRegistration/base/forms.py
@@ -38,3 +38,42 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ['first_mame', 'last_name', 'username', 'email', 'password1', 'password2']
         
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                                widget=forms.TextInput(attrs={'placeholder': 'Username',
+                                                              'class': 'form-control',}) )
+    password = forms.CharField(max_length=50, required=True,
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Password',
+                                                                 'class': 'form-control',
+                                                                 'data-toggle': 'password',
+                                                                 'id': 'password,',
+                                                                 'name': 'password',}))
+    remember_me = forms.BooleanField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'remember_me']
+
+    
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=True,
+                             widget=forms.TextInput(attrs={'class': 'form-control'}))
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class UpdateProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
