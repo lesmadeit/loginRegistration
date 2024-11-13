c5af7699059803af7d0ac55d92ddcbbffd1371e8
lesmadeit <lesmwendwa@gmail.com>
2024-11-13 21:46:47 +0300

    changes

--- loginRegistration/base/urls.py
+++ loginRegistration/base/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from .views import authView, home
+from .views import  home, profile, RegisterView
 
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("signup", authView, name="authView"),
-    path("accounts", include("django.contrib.auth.urls")),
+    path('', home, name="base-home"),
+    path('register/', RegisterView.as_view(), name="users-register"),
+    path('profile/', profile, name='user-profile'),
 ]
---
+++ loginRegistration/base/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordChangeView
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views import View
+from django.contrib.auth.decorators import login_required
+
+from .forms import RegisterForm, LoginForm, UpdateUserForm, UpdateProfileForm
+
+
+def home(request):
+    return render(request, 'user/home.html')
+
